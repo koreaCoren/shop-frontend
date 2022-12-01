e6d93da236ea66ea1395985d75839a0bbd935b2d
zyakal <pkd1426@gmail.com>
2022-12-01 09:33:13 +0900

    12.01

--- src/utils/apis.js
+++ src/utils/apis.js
@@ -2,10 +2,10 @@ import axios from "axios";
 
 //토큰 api
 export const token = axios.create({
-    baseURL: `${process.env.REACT_APP_API_URL}/user/break_token`,
+    baseURL: `/user/break_token`,
 })
 
 //로그인 관련 api
 export const loginApi = axios.create({
-    baseURL: `${process.env.REACT_APP_API_URL}/user/sel_user`,
+    baseURL: `/user/sel_user`,
 });
