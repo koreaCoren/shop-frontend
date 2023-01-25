0855b865882e5d0c1206779fd660d8fd50dbd222
jung woo Kwon <kwog5050@naver.com>
2023-01-25 18:04:51 +0900

    주문하기

--- src/pages/product/Detail.js
+++ src/pages/product/Detail.js
@@ -9,6 +9,7 @@ const Detail = ({ result, setOrderData }) => {
     const { productCode } = useParams();
     const [productDetail, setProductDetail] = useState();
     const [count, setCount] = useState(1);
+    const [deliveryPay, setDeliveryPay] = useState(2500);
 
     //해당 페이지 상품 디테일 가져오기
     useEffect(() => {
@@ -19,6 +20,7 @@ const Detail = ({ result, setOrderData }) => {
                 };
             };
         };
+        console.log(productDetail);
     }, [result.isLoading]);
 
     //상품갯수증가
@@ -39,9 +41,11 @@ const Detail = ({ result, setOrderData }) => {
     const orderClick = () => {
         const data = {
             product_code: productCode,
-            price_name: productDetail?.goods_nm,
+            product_name: productDetail?.goods_nm,
             product_img: productDetail?.goods_img,
+            deliveryPay: deliveryPay,
             price: productDetail?.goods_price,
+            sale: productDetail?.goods_sale,
             prodcut_count: count,
             total_price: Math.ceil(productDetail?.goods_price - (productDetail?.goods_price * (productDetail?.goods_sale * 0.01))) * count
         }
@@ -69,7 +73,7 @@ const Detail = ({ result, setOrderData }) => {
                                 <li><b>할인률</b><span>{productDetail?.goods_sale}%</span></li>
                                 <li><b>국내해외배송</b><span>국내배송</span></li>
                                 <li><b>배송방법</b><span>택배</span></li>
-                                <li><b>배송비</b><span>2,500 (50,000 이상 구매시 무료)</span></li>
+                                <li><b>배송비</b><span>{deliveryPay} (50,000 이상 구매시 무료)</span></li>
                             </Style.DetailInfo>
                             <p>(최소주문수량 1개 이상)</p>
                             <Style.Quantity>
