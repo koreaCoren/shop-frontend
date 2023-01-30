315c90585dc251fbd8a3f66a1682d091bb7b71c0
jung woo Kwon <kwog5050@naver.com>
2023-01-30 16:30:51 +0900

    어드민 주문관리 배송관리 페이지 추가

--- src/components/admin/sideMenu/data.js
+++ src/components/admin/sideMenu/data.js
@@ -5,10 +5,19 @@ const menus = [
             { title: "카테고리 추가/삭제", url: "Category" },
             { title: "상품등록", url: "product" }
         ],
-        url: [
-            "",
-            "product"
-        ]
+        icon: "fa-solid fa-gift",
+    },
+    {
+        mainTitle: "주문 관리",
+        subList: [],
+        url: "order",
+        icon: "fa-solid fa-folder-tree",
+    },
+    {
+        mainTitle: "배송 관리",
+        subList: [],
+        url: "delivery",
+        icon: "fa-solid fa-truck",
     }
 ]
 
---
+++ src/pages/admin/delivery/Delivery.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Delivery = () => {
+    return (
+        <>
+            배송관리
+        </>
+    );
+};
+
+export default Delivery;
