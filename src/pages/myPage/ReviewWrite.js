b874387aa337e24e9b0ab114efc06125cbf0e81a
zyakal <pkd1426@gmail.com>
2023-03-27 15:40:29 +0900

    0327 리뷰 관련 수정

--- src/pages/myPage/ReviewWrite.js
+++ src/pages/myPage/ReviewWrite.js
@@ -10,7 +10,6 @@ import StartGrade from 'components/input/StarGrade';
 
 import * as Common from "assets/styleComponent/myPage/myPage"
 import ImageUpload from 'components/input/ImageUpload';
-import { useEffect } from 'react';
 
 import noImg from "assets/images/noImg.gif";
 
@@ -22,11 +21,11 @@ const ReviewWrite = () => {
     const [firstImage, setFirstImage] = useState("");
     const [secondImage, setSecondImage] = useState("");
     const [star, setStar] = useState(5);
-    
+
     const location = useLocation();
     const goods_name = location.state.goods_name;
     const goods_img = location.state.goods_img;
-    
+
 
     const write = useMutation(boardWrite);
     const token = useMutation(tokenCheck);
@@ -80,20 +79,20 @@ const ReviewWrite = () => {
     return (
         <Common.InDiv>
             <SubTitle h2={"상품후기 작성"} h3={"구매하신 상품후기를 작성 하실 수 있습니다."} clickEvent={null} clickText={null} />
-            
+
             <Common.ReviewTitle>
                 <div>
                     <img src={
-                                goods_img === ""
-                                    ? noImg
-                                    : goods_img
+                        goods_img === ""
+                            ? noImg
+                            : goods_img
                     } alt="" />
                 </div>
                 <div><p>{goods_name}</p></div>
-                
+
             </Common.ReviewTitle>
             <Common.ReviewForm onSubmit={onSubmit}>
-                <StartGrade settingStar={settingStar}/>
+                <StartGrade settingStar={settingStar} />
                 <LoginInput maxLength={100} type="text" name="title" value={title} placeholder="제목 최대 100자" onChange={onChange}></LoginInput>
                 <textarea maxLength={1000} name="content" value={content} onChange={onChange} placeholder="내용 최대 1000자"></textarea>
                 <ImageUpload setImageData={setFirstImage} />
