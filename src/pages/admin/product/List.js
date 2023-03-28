5daae3b8f335fc1c592854a0c43d6d8e1f3719de
zyakal <pkd1426@gmail.com>
2023-03-28 13:16:47 +0900

    0328

--- src/pages/admin/product/List.js
+++ src/pages/admin/product/List.js
@@ -24,42 +24,43 @@ const List = () => {
     }
 
     return (
-        <>
-            {
-                result.data?.map((a, i) => {
-                    return (
-                        <Common.Container key={i}>
-                            <ul>
-                                <Style.ProductList>
-                                    <img src={a.goods_img === "" ? noImg : a.goods_img} alt="" />
-                                    <Style.Div>
-                                        <ul>
-                                            <li>상품이름 : {a.goods_nm}</li>
-                                            <li>가격 : {a.goods_price}원</li>
-                                            <li>할인률 : {a.goods_sale}%</li>
-                                        </ul>
-                                        <ul>
-                                            <li>판매 수량 : {a.goods_sell}</li>
-                                            <li>재고 : {a.goods_stock}</li>
-                                            {
-                                                a.goods_stock == 0
-                                                    ? <li className='warning'>재고 부족!</li>
-                                                    : ""
-                                            }
-                                        </ul>
-                                    </Style.Div>
-                                    <Style.Ul>
-                                        <li><Link to={`modfiy/${a.goods_code}`} >수정</Link></li>
-                                        <li><button type='button' onClick={() => { onDelete(a) }}>삭제</button></li>
-                                    </Style.Ul>
-                                </Style.ProductList>
-                            </ul>
-                        </Common.Container>
-                    )
-                })
-            }
-            {result.isLoading && <Loading />}
-        </>
+        result.isLoading === true
+            ? <Loading />
+            : <>
+                {
+                    result.data[0].map((a, i) => {
+                        return (
+                            <Common.Container key={i}>
+                                <ul>
+                                    <Style.ProductList>
+                                        <img src={a.goods_img === "" ? noImg : a.goods_img} alt="" />
+                                        <Style.Div>
+                                            <ul>
+                                                <li>상품이름 : {a.goods_nm}</li>
+                                                <li>가격 : {a.goods_price}원</li>
+                                                <li>할인률 : {a.goods_sale}%</li>
+                                            </ul>
+                                            <ul>
+                                                <li>판매 수량 : {a.goods_sell}</li>
+                                                <li>재고 : {a.goods_stock}</li>
+                                                {
+                                                    a.goods_stock == 0
+                                                        ? <li className='warning'>재고 부족!</li>
+                                                        : ""
+                                                }
+                                            </ul>
+                                        </Style.Div>
+                                        <Style.Ul>
+                                            <li><Link to={`modfiy/${a.goods_code}`} >수정</Link></li>
+                                            <li><button type='button' onClick={() => { onDelete(a) }}>삭제</button></li>
+                                        </Style.Ul>
+                                    </Style.ProductList>
+                                </ul>
+                            </Common.Container>
+                        )
+                    })
+                }
+            </>
     );
 };
 
