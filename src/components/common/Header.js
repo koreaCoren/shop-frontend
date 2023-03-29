12bb74a28c141aca0733e72aebd92164f32d58c9
jung woo Kwon <kwog5050@naver.com>
2023-03-29 16:42:19 +0900

    헤더 모바일 추가

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useQuery } from 'react-query';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 import { categoryList, logout } from 'utils/axios';
 import Loading from 'components/loding/Loading';
@@ -10,16 +10,25 @@ import styled from 'styled-components';
 import logo from "assets/images/logo.png"
 
 const Header = () => {
+    const nav = useNavigate();
     const [categorys, setCategorys] = useState([]);
+    const [isMobileMenu, setIsMobileMenu] = useState(false);
+    const [mobileMenuNum, setMobileMenuNum] = useState(0);
+    const [isMenuBox, setIsMenuBox] = useState(false);
     const result = useQuery("categoryList", categoryList);
 
     useEffect(() => {
         setCategorys(result.data);
     }, [result.isLoading])
 
+    useEffect(() => {
+        setIsMobileMenu(false);
+        setIsMenuBox(false);
+    }, [nav])
+
     return (
         <header style={{ boxShadow: "0px 0px 5px #00000033" }}>
-            <Login>
+            <Login className='pc'>
                 <div className="wrap">
                     {
                         sessionStorage.getItem("loginCheck") === "success"
@@ -52,7 +61,7 @@ const Header = () => {
                 <div className="wrap">
                     <div className="flexBox">
                         <a href="/"><h1><img src={logo} alt="" /></h1></a>
-                        <nav>
+                        <nav className='pc'>
                             <ul>
                                 {
                                     categorys?.map((a, i) => {
@@ -76,9 +85,89 @@ const Header = () => {
                                 <li><Link to={"community/notice/1"}>공지사항</Link></li>
                             </ul>
                         </nav>
+
+                        <div className="mobile">
+                            <button onClick={() => { setIsMenuBox(!isMenuBox) }}>
+                                <i className="fa-solid fa-bars"></i>
+                            </button>
+
+                            <div className={
+                                isMenuBox === true
+                                    ? "menuBox on"
+                                    : "menuBox"
+                            }>
+                                {
+                                    sessionStorage.getItem("loginCheck") === "success"
+                                        ? <ul className='user flexBox'>
+                                            <li>{sessionStorage.getItem("userId")}님 로그인중</li>
+                                            <li>로그아웃</li>
+                                        </ul>
+                                        : <ul className='user'>
+                                            <li>로그인</li>
+                                        </ul>
+                                }
+
+                                <ul className='nav'>
+                                    <li>
+                                        <Link to={"product/basket"}>
+                                            <i className="fa-solid fa-basket-shopping"></i>
+                                            장바구니
+                                        </Link>
+                                    </li>
+                                    {
+                                        sessionStorage.getItem("userId")
+                                        && <li>
+                                            <Link to={"myPage/order/1"}>
+                                                <i className="fa-solid fa-user"></i>
+                                                마이페이지
+                                            </Link>
+                                        </li>
+                                    }
+                                </ul>
+
+                                <ul className='menu'>
+                                    {
+                                        categorys?.map((a, i) => {
+                                            return (
+                                                <li key={i}>
+                                                    {
+                                                        a.lowCategory.length <= 0
+                                                            ? <Link to={`/product/products/${(a.cate_code)}/1`}>{a.cate}</Link>
+                                                            : <span
+                                                                onClick={() => {
+                                                                    setIsMobileMenu(!isMobileMenu);
+                                                                    setMobileMenuNum(i);
+                                                                }}
+                                                            >{a.cate}</span>
+                                                    }
+                                                    <ol style={
+                                                        isMobileMenu === true
+                                                            ? mobileMenuNum === i
+                                                                ? { maxHeight: `${a.lowCategory.length * 40}px` }
+                                                                : { maxHeight: `0px` }
+                                                            : { maxHeight: `0px` }
+                                                    }>
+                                                        {
+                                                            a.lowCategory?.map((b, j) => {
+                                                                return (
+                                                                    <li key={j}><Link to={`/product/products/${b.cate_code}/1`}>{b.cate}</Link></li>
+                                                                )
+                                                            })
+                                                        }
+                                                    </ol>
+                                                </li>
+                                            )
+                                        })
+                                    }
+                                    <li><Link to={"community/review/all/1"}>리뷰보기</Link></li>
+                                    <li><Link to={"community/notice/1"}>공지사항</Link></li>
+                                </ul>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </Head>
+
 
             {result.isLoading && <Loading />}
         </header>
@@ -132,6 +221,7 @@ const Head = styled.div`
     }
 
     nav ul  li ol li{
+        width: 100%;
     }
 
     nav ul li ol li:hover{
@@ -139,11 +229,116 @@ const Head = styled.div`
     }
 
     nav ul li ol li a{
+        width: 100%;
         font-size: 16px;
         white-space:nowrap;
         line-height: 40px;
         padding: 0px 10px;
+        text-align: center;
     }
+
+    .mobile button i{
+        font-size: 24px;
+        cursor: pointer;
+        line-height: 50px;
+    }
+
+    .mobile .menuBox{
+        position: fixed;
+        top: 0;
+        left: 0;
+        background-color: #fff;
+        width: 250px;
+        max-width : 0px;
+        overflow: hidden;
+        transition: max-width .3s, opacity .2s;
+        height: 100vh;
+        z-index: 10;
+        opacity: 0;
+    }
+
+    .mobile .menuBox.on{
+        opacity: 1;
+        max-width: 250px;
+    }
+
+    .mobile .menuBox ul.user{
+        padding: 10px 5px;
+        border-bottom: 1px solid #ccc;
+    }
+
+    .mobile .menuBox ul.user li{
+        white-space: nowrap;
+    }
+
+    .mobile .menuBox ul.nav{
+        display: flex;
+    }
+
+    .mobile .menuBox ul.nav li{
+        flex: 1;
+        text-align: center;
+        padding: 10px 0px;
+        border-bottom: 1px solid #ccc;
+    }
+
+    .mobile .menuBox ul.nav li a{
+        display: flex;
+        flex-direction: column;
+        gap: 5px;
+        white-space: nowrap;
+    }
+
+    .mobile .menuBox ul.nav li i{
+        font-size: 20px;
+    }
+
+    .mobile .menuBox ul.nav li:nth-child(1){
+        border-right: 1px solid #ccc;
+    }
+
+    .mobile .menuBox ul.menu{
+        display: flex;
+        flex-direction: column;
+    }
+
+    .mobile .menuBox ul.menu li{
+        width: 100%;
+    }
+
+    .mobile .menuBox ul.menu > li > span,
+    .mobile .menuBox ul.menu > li > a{
+        display: inline-block;
+        width: 100%;
+        border-bottom: 1px solid #ccc;
+        text-align: center;
+        line-height: 40px;
+        white-space: nowrap;
+        cursor: pointer;
+    }
+
+    .mobile .menuBox ul.menu > li > ol{
+        background-color: #eee;
+        max-height: 0px;
+        overflow: hidden;
+        transition: max-height .5s;
+    }
+
+    .mobile .menuBox ul.menu > li > ol > li{
+        width: 100%;
+    }
+
+    .mobile .menuBox ul.menu > li > ol > li > a{
+        text-align: center;
+        line-height:40px;
+        width: 100%;
+        white-space: nowrap;
+    }
+
+    .mobile .menuBox ul.menu > li > ol > li:not(:last-child) > a{
+        border-bottom: 1px solid #ccc;
+    }
+
 `
 
 
