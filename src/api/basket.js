91bd48c7e2ece43565612ef15eb11f3be4fc8452
jung woo Kwon <kwog5050@naver.com>
2023-05-09 10:30:25 +0900

    장바구니

---
+++ src/api/basket.js
@@ -0,0 +1,14 @@
+import axios from './axios';
+import { handleApiError } from './handleError';
+
+// 장바구니 상품 정보 가져오기
+const getBasket = async (data, success) => {
+    try {
+        const res = await axios.post('/?/?', data);
+        success(res.data);
+    } catch (error) {
+        handleApiError(error);
+    }
+};
+
+export { getBasket };
