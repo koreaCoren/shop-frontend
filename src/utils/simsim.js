013b3a41b15d490767c062835d9eed4a5e761cba
jung woo Kwon <kwog5050@naver.com>
2023-06-15 16:51:04 +0900

    원하는대로해줌

--- src/utils/simsim.js
+++ src/utils/simsim.js
@@ -2,50 +2,49 @@ const sim = () => {
     const body = document.querySelector("body");
     const color = ["red", "orange", "yellow", "green", "blue", "indigo", "purple"];
     let index = 0;
+    let x = 0;
+    let y = 0;
+
     function randomValueFromArray(array) {
         const random = Math.floor(Math.random() * array.length);
         return array[random];
     }
+
     window.addEventListener("load", () => {
         const zz = document.createElement("div");
         body.append(zz);
         index++;
-        zz.style = `position: fixed;
-        top: 50%;
-        left: 50%;
-        width: 100%;
-        height: 100vh;
-        transform: translate(-50%, -50%);
-        background-color :${randomValueFromArray(color)};
-        z-index: ${index};
-        `
-        setInterval(() => {
-            zz.style.backgroundColor = randomValueFromArray(color);
-        }, 100);
+
+        window.addEventListener('mousemove', (e) => {
+            x = e.clientX;
+            y = e.clientY;
+        });
+
+        window.addEventListener("click", () => {
+            const zz = document.createElement("div");
+            body.append(zz);
+            index++;
+            zz.style = `position: fixed;
+            top: ${y}px;
+            left: ${x}px;
+            width: 0px;
+            height: 0px;
+            border-radius: 50%;
+            transform: translate(-50%, -50%);
+            background-color :${randomValueFromArray(color)};
+            transition: all 2s;
+            z-index: ${index};
+            `
+            setTimeout(() => {
+                zz.style.width = "10000px";
+                zz.style.height = "10000px";
+                zz.style.borderRadius = "0px";
+                zz.style.opacity = "0.5";
+            }, 500);
+        })
     })
 
-    window.addEventListener("click", () => {
-        const zz = document.createElement("div");
-        body.append(zz);
-        index++;
-        zz.style = `position: fixed;
-        top: 50%;
-        left: 50%;
-        width: 50px;
-        height: 50px;
-        transform: translate(-50%, -50%);
-        border-radius: 50%;
-        background-color :${randomValueFromArray(color)};
-        transition: all .5s;
-        z-index: ${index};
-        `
-        setTimeout(() => {
-            zz.style.width = "100%";
-            zz.style.height = "100vh";
-            zz.style.borderRadius = "0px";
-            zz.style.opacity = "0.5";
-        }, 500);
-    })
+
 }
 
 export default sim;
