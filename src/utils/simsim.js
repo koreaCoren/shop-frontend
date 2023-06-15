1a6f60169260cea53867c862c4f05b7339774dc6
jung woo Kwon <kwog5050@naver.com>
2023-06-15 17:18:59 +0900

    재밌군요

--- src/utils/simsim.js
+++ src/utils/simsim.js
@@ -1,6 +1,6 @@
 const sim = () => {
     const body = document.querySelector("body");
-    const color = ["red", "orange", "yellow", "green", "blue", "indigo", "purple"];
+    let isMouse = false;
     let index = 0;
     let x = 0;
     let y = 0;
@@ -18,28 +18,38 @@ const sim = () => {
             y = e.clientY;
         });
 
-        window.addEventListener("click", () => {
-            const zz = document.createElement("div");
-            body.append(zz);
-            index++;
-            zz.style = `position: fixed;
-            top: ${y}px;
-            left: ${x}px;
-            width: 0px;
-            height: 0px;
-            border-radius: 50%;
-            transform: translate(-50%, -50%);
-            background-color :#${generateRandomSequence()};
-            transition: all .5s;
-            z-index: ${index};
-            `
-            setTimeout(() => {
-                zz.style.width = "200px";
-                zz.style.height = "200px";
-                zz.style.borderRadius = "0px";
-                zz.style.opacity = "0.5";
-            }, 0);
-        })
+        window.addEventListener('mousedown', () => {
+            isMouse = true;
+            window.addEventListener('mousemove', (e) => {
+                x = e.clientX;
+                y = e.clientY;
+                if (isMouse) {
+                    const zz = document.createElement("div");
+                    body.append(zz);
+                    index++;
+                    zz.style = `position: fixed;
+                    top: ${y}px;
+                    left: ${x}px;
+                    width: 0px;
+                    height: 0px;
+                    border-radius: 50%;
+                    transform: translate(-50%, -50%);
+                    background-color :#${generateRandomSequence()};
+                    transition: all .5s;
+                    z-index: ${index};
+                    `
+                    setTimeout(() => {
+                        zz.style.width = "20px";
+                        zz.style.height = "20px";
+                        zz.style.opacity = "0.5";
+                    }, 0);
+                }
+            });
+        });
+
+        window.addEventListener('mouseup', function (event) {
+            isMouse = false;
+        });
     })
 }
 
