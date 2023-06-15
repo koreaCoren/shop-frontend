86c096b5a9dc1a0902129f8b18e2069b60c9a7a4
jung woo Kwon <kwog5050@naver.com>
2023-06-15 16:31:34 +0900

    simsim

--- src/utils/simsim.js
+++ src/utils/simsim.js
@@ -6,28 +6,46 @@ const sim = () => {
         const random = Math.floor(Math.random() * array.length);
         return array[random];
     }
-    window.addEventListener("click", () => {
+    window.addEventListener("load", () => {
         const zz = document.createElement("div");
         body.append(zz);
         index++;
         zz.style = `position: fixed;
         top: 50%;
         left: 50%;
-        width: 50px;
-        height: 50px;
+        width: 100%;
+        height: 100vh;
         transform: translate(-50%, -50%);
-        border-radius: 50%;
         background-color :${randomValueFromArray(color)};
-        transition: all .5s;
         z-index: ${index};
         `
-        setTimeout(() => {
-            zz.style.width = "100%";
-            zz.style.height = "100vh";
-            zz.style.borderRadius = "0px";
-            zz.style.opacity = "0.5";
-        }, 500);
+        setInterval(() => {
+            zz.style.backgroundColor = randomValueFromArray(color);
+        }, 100);
     })
+
+    // window.addEventListener("click", () => {
+    //     const zz = document.createElement("div");
+    //     body.append(zz);
+    //     index++;
+    //     zz.style = `position: fixed;
+    //     top: 50%;
+    //     left: 50%;
+    //     width: 50px;
+    //     height: 50px;
+    //     transform: translate(-50%, -50%);
+    //     border-radius: 50%;
+    //     background-color :${randomValueFromArray(color)};
+    //     transition: all .5s;
+    //     z-index: ${index};
+    //     `
+    //     setTimeout(() => {
+    //         zz.style.width = "100%";
+    //         zz.style.height = "100vh";
+    //         zz.style.borderRadius = "0px";
+    //         zz.style.opacity = "0.5";
+    //     }, 500);
+    // })
 }
 
 export default sim;
