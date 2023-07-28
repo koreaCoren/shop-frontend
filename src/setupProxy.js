3e88c3338d02d41360e71cd39444954c518313e8
zyakal <pkd1426@gmail.com>
2023-07-28 14:52:54 +0900

    .

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -4,8 +4,8 @@ module.exports = function (app) {
     app.use(
         "/backend",
         createProxyMiddleware({
-            // target: "https://lifehim.com",
-            target: "https://localhost",
+            target: "https://lifehim.com",
+            // target: "https://localhost",
             changeOrigin: true,
         })
     );
